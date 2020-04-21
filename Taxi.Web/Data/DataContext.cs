186573f7712c7c5c09cb6599de3be240f1b8e203
Vladimir Miranda <vladi_vinci@hotmail.com>
2020-04-21 16:39:34 -0500

    Ingreso de los viajes

--- Taxi.Web/Data/DataContext.cs
+++ Taxi.Web/Data/DataContext.cs
@@ -10,6 +10,11 @@ namespace Taxi.Web.Data
     public class DataContext : DbContext
     {
         public DbSet<TaxiEntity> Taxis { get; set; }
+
+        public DbSet<TripEntity> Trips { get; set; }
+
+        public DbSet<TripDetailEntity> TripDetails { get; set; }
+
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
